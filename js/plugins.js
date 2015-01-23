44e02e1a642f68df8b07b5e01a60164ac3466b6f
Michael Nino Evensen <michaelevensen@gmail.com>
2015-01-23 14:53:30 +0100

    first

---
+++ js/plugins.js
@@ -0,0 +1,44 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+// images
+$(function(){
+    $('.images img:gt(0)').hide();
+
+    // fadeinout time
+    var t = 200;
+
+    // click thru images
+    $('.images').click(function() {
+      $('.images :first-child').fadeOut(t)
+         .next('img').fadeIn(t)
+          .end().appendTo('.images'); 
+    });
+});
+
+// about
+$('.logo').click(function() {
+    $('.images').toggle();
+  $('.about').toggle();
+});
